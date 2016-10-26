eeec6a9217d92a5e9f8fef19fa4dfb8b8978f0df
DanielZelei <zelei.dan@gmail.com>
2016-10-26 22:46:19 +0200

    init spring boot project

---
+++ src/main/java/com/gomoku/FiveInARowChallengeRandomAiApplication.java
@@ -0,0 +1,12 @@
+package com.gomoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveInARowChallengeRandomAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveInARowChallengeRandomAiApplication.class, args);
+	}
+}
